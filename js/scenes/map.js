6c06593bde3bcd6146ba56ed13bb26e91caafc92
Kevin Kub <kevin@Kevins-MacBook-Air.local>
2014-11-14 21:58:43 +0100

    Kevin erster Commit

---
+++ js/scenes/map.js
@@ -0,0 +1,19 @@
+function mapScene() {
+	//this.bg = new sprite("mock/map.png");
+	
+	// Add buttons to button bar
+	// var buttonBar = new ButtonBar(
+	// 	new Button(...), 
+	// 	new Button(...), 
+	// 	new Button(...)
+	//);
+	// Add button bar
+	//this.entities.push();
+		
+	// Add encounters
+	//for(var encounter in encounters) {
+	//}
+
+}
+
+mapScene.prototype = new scene();
