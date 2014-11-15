13e65a6a9cffc8b8735b20ed11dc822b5bbe1028
Tobias Rojahn <tr@wlec.ag>
2014-11-15 13:38:09 +0100

    roboter

--- js/entities/skills.js
+++ js/entities/skills.js
@@ -5,11 +5,13 @@ function Attack( scene, actor ) {
 
 Attack.prototype.run = function( target ) {
 	this.scene.blocking.push( new MoveTwards(this.actor, target.x-100, target.y, this.actor.x, this.actor.y, 500 ));
+	var actorStats = this.actor.getStats();
 
-	var hit = ( 0.9 * this.actor.getStats().dex / target.getStats().dex ) > Math.random();
+	var hit = ( 0.9 * actorStats.dex / target.getStats().dex ) > Math.random();
 	if( hit ) {
+		var damage = actorStats.str + Math.round(( actorStats.str * 0.6 ) * (Math.random() - .5));
 		this.scene.blocking.push( new Animation(target.x, target.y, 'img/animation/strike1.png', 5, 100))
-		this.scene.blocking.push( new AnimationDamage( target, 555 ));
+		this.scene.blocking.push( new AnimationDamage( target, damage ));
 	} else {
 		this.scene.blocking.push(new AnimationText(target, 'Miss'));
 	}
