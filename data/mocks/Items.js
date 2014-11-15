6da91ff4557940f34fcf7769c2e201a427326a97
Trokan <mail@simonpontius.de>
2014-11-15 23:57:30 +0100

    Items.js balance update

--- data/mocks/Items.js
+++ data/mocks/Items.js
@@ -3,21 +3,21 @@ var itemDefinitions={
 		"itemname":"Stärke-Amulett",
 		"sprite":"img/items/neck.png",
 		"slot":"neck",
-		"str":1,
+		"str":5,
 		"def":1,
 		"int":1,
 		"dex":1,
-		"blingbling":1
+		"blingbling":10
 	},
 	"stdneckdef":{
 		"itemname":"Panzer-Amulett",
 		"sprite":"img/items/neck.png",
 		"slot":"neck",
 		"str":1,
-		"def":1,
+		"def":5,
 		"int":1,
 		"dex":1,
-		"blingbling":1
+		"blingbling":10
 	},
 	"stdneckdex":{
 		"itemname":"Geschicklichkeit-Amulett",
@@ -26,8 +26,8 @@ var itemDefinitions={
 		"str":1,
 		"def":1,
 		"int":1,
-		"dex":1,
-		"blingbling":1
+		"dex":5,
+		"blingbling":10
 	},
 	"stdneckint":{
 		"itemname":"Intelligenz-Amulett",
@@ -35,69 +35,69 @@ var itemDefinitions={
 		"slot":"neck",
 		"str":1,
 		"def":1,
-		"int":1,
+		"int":5,
 		"dex":1,
-		"blingbling":1
+		"blingbling":10
 	},
 	"stdbootsstr":{
 		"itemname":"Stärke-Schuhe",
 		"sprite":"img/items/boots.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":10,
+		"def":2,
+		"int":2,
+		"dex":2,
+		"blingbling":5
 	},
 	"stdbootsdef":{
 		"itemname":"Panzer-Schuhe",
 		"sprite":"img/items/boots.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":2,
+		"def":10,
+		"int":2,
+		"dex":2,
+		"blingbling":5
 	},
 	"stdbootsdex":{
 		"itemname":"Geschicklichkeit-Schuhe",
 		"sprite":"img/items/boots.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":2,
+		"def":2,
+		"int":2,
+		"dex":10,
+		"blingbling":5
 	},
 	"stdbootsint":{
 		"itemname":"Intelligenz-Schuhe",
 		"sprite":"img/items/boots.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":2,
+		"def":2,
+		"int":10,
+		"dex":2,
+		"blingbling":5
 	},
 	"stdringstr":{
 		"itemname":"Stärke-Ring",
 		"sprite":"img/items/ring.png",
 		"slot":"ring",
-		"str":1,
+		"str":5,
 		"def":1,
 		"int":1,
 		"dex":1,
-		"blingbling":1
+		"blingbling":10
 	},
 	"stdringdef":{
 		"itemname":"Panzer-Ring",
 		"sprite":"img/items/ring.png",
 		"slot":"ring",
 		"str":1,
-		"def":1,
+		"def":5,
 		"int":1,
 		"dex":1,
-		"blingbling":1
+		"blingbling":10
 	},
 	"stdringdex":{
 		"itemname":"Geschicklichkeit-Ring",
@@ -106,8 +106,8 @@ var itemDefinitions={
 		"str":1,
 		"def":1,
 		"int":1,
-		"dex":1,
-		"blingbling":1
+		"dex":5,
+		"blingbling":10
 	},
 	"stdringint":{
 		"itemname":"Intelligenz-Ring",
@@ -115,169 +115,169 @@ var itemDefinitions={
 		"slot":"ring",
 		"str":1,
 		"def":1,
-		"int":1,
+		"int":5,
 		"dex":1,
-		"blingbling":1
+		"blingbling":10
 	},
 	"stdcheststr":{
 		"itemname":"Stärke-Rüstung",
 		"sprite":"img/items/chest.png",
 		"slot":"chest",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":10,
+		"def":3,
+		"int":3,
+		"dex":3,
+		"blingbling":
 	},
 	"stdchestdef":{
 		"itemname":"Panzer-Rüstung",
 		"sprite":"img/items/chest.png",
 		"slot":"chest",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":3,
+		"def":10,
+		"int":3,
+		"dex":3,
+		"blingbling":5
 	},
 	"stdchestdex":{
 		"itemname":"Geschicklichkeit-Rüstung",
 		"sprite":"img/items/chest.png",
 		"slot":"chest",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":3,
+		"def":3,
+		"int":3,
+		"dex":10,
+		"blingbling":5
 	},
 	"stdchestint":{
 		"itemname":"Intelligenz-Rüstung",
 		"sprite":"img/items/chest.png",
 		"slot":"chest",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":3,
+		"def":3,
+		"int":10,
+		"dex":3,
+		"blingbling":5
 	},
 	"stdhelmetstr":{
 		"itemname":"Stärke-Helm",
 		"sprite":"img/items/helmet.png",
 		"slot":"helmet",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":10,
+		"def":2,
+		"int":2,
+		"dex":2,
+		"blingbling":5
 	},
 	"stdhelmetdef":{
 		"itemname":"Panzer-Helm",
 		"sprite":"img/items/helmet.png",
 		"slot":"helmet",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":2,
+		"def":10,
+		"int":2,
+		"dex":2,
+		"blingbling":5
 	},
 	"stdhelmetdex":{
 		"itemname":"Geschicklichkeit-Helm",
 		"sprite":"img/items/helmet.png",
 		"slot":"helmet",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":2,
+		"def":2,
+		"int":2,
+		"dex":10,
+		"blingbling":5
 	},
 	"stdhelmetint":{
 		"itemname":"Intelligenz-Helm",
 		"sprite":"img/items/helmet.png",
 		"slot":"helmet",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":2,
+		"def":2,
+		"int":10,
+		"dex":2,
+		"blingbling":5
 	},
 	"klosteinstr":{
 		"itemname":"Klostein Amulett",
 		"sprite":"img/items/klostein.png",
 		"slot":"neck",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":15,
+		"def":5,
+		"int":5,
+		"dex":5,
+		"blingbling":20
 	},
 	"klosteindef":{
 		"itemname":"Klostein Amulett",
 		"sprite":"img/items/klostein.png",
 		"slot":"neck",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":5,
+		"def":15,
+		"int":5,
+		"dex":5,
+		"blingbling":20
 	},
 	"klosteindex":{
 		"itemname":"Klostein Amulett",
 		"sprite":"img/items/klostein.png",
 		"slot":"neck",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":5,
+		"def":5,
+		"int":5,
+		"dex":15,
+		"blingbling":20
 	},
 	"klosteinint":{
 		"itemname":"Klostein Amulett",
 		"sprite":"img/items/klostein.png",
 		"slot":"neck",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":5,
+		"def":5,
+		"int":15,
+		"dex":5,
+		"blingbling":20
 	},
 	"sandalensockenstr":{
 		"itemname":"Sandalen mit Socken",
 		"sprite":"img/items/sandalensocken.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":20,
+		"def":5,
+		"int":5,
+		"dex":5,
+		"blingbling":20
 	},
 	"sandalensockendef":{
 		"itemname":"Sandalen mit Socken",
 		"sprite":"img/items/sandalensocken.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":5,
+		"def":20,
+		"int":5,
+		"dex":5,
+		"blingbling":20
 	},
 	"sandalensockendex":{
 		"itemname":"Sandalen mit Socken",
 		"sprite":"img/items/sandalensocken.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":5,
+		"def":5,
+		"int":5,
+		"dex":20,
+		"blingbling":20
 	},
 	"sandalensockenint":{
 		"itemname":"Sandalen mit Socken",
 		"sprite":"img/items/sandalensocken.png",
 		"slot":"boots",
-		"str":1,
-		"def":1,
-		"int":1,
-		"dex":1,
-		"blingbling":1
+		"str":5,
+		"def":5,
+		"int":20,
+		"dex":5,
+		"blingbling":20
 	},
 	"damfhammer":{
 		"itemname":"Dampfhammeruhr 3000",
@@ -287,7 +287,7 @@ var itemDefinitions={
 		"def":1,
 		"int":1,
 		"dex":1,
-		"blingbling":1
+		"blingbling":20
 	},
 	"potion":{
 		"itemname":"Heiltrank",
