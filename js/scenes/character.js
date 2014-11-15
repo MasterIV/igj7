86cd94738463ca996e8d26c0f277b363fb9990c7
purhur <purhur@purh.pw>
2014-11-15 14:14:39 +0100

    eq

--- js/scenes/character.js
+++ js/scenes/character.js
@@ -1,6 +1,7 @@
 function characterScene() {
 	this.entities = [
 		new stats(600, 20),
+		new HeroContainer(200,200),
 	];
 
 	this.initDropareas();
@@ -27,28 +28,48 @@ characterScene.prototype.initDropareas = function () {
 		this.itemslots.push(is);
 	}
 
-	this.entities.push(new equipslot(x,y + 130,120,120,'weapon',function (item) {
-			hero.equip(item.item);
-		}, function (item) {
-			hero.unequip(item.item);
-		})
-	);
-	this.entities.push(new equipslot(x+130,y + 130,120,120,'shield',function (item) {
-			hero.equip(item.item);
-		}, function (item) {
-			hero.unequip(item.item);
-		})
-	);
+	this.equipmentslots = {};
+
+	var helmet = new equipslot(x,y + 130,120,120,'weapon',function (item) {
+		hero.equip(item.item);
+	}, function (item) {
+		hero.unequip(item.item);
+	});
+
+	var ring = new equipslot(x+130,y + 130,120,120,'shield',function (item) {
+		hero.equip(item.item);
+	}, function (item) {
+		hero.unequip(item.item);
+	});
+
+	this.equipmentslots.helmet = helmet;
+	this.equipmentslots.ring = ring;
+	this.entities.push(helmet, ring);
 }
 
 characterScene.prototype.initItems = function () {
+	// Inventory
 	var items = hero.getInventory();
 
 	for(var i =0;i<items.length;i++) {
-
 		var dg = new dragable(new itemContainer(this.itemslots[i].area.p1.x+this.itemslots[i].padding.x,this.itemslots[i].area.p1.y +this.itemslots[i].padding.y,100,100,items[i]), items[i].itemdefinition.type);
 
 		this.itemslots[i].setContent(dg)
 		this.entities.push(dg);
 	}
+
+	//Equipment
+
+	for(var item in hero.equipment) {
+		if (hero.equipment[item] != null) {
+
+			var dg = new dragable(new itemContainer(this.equipmentslots[hero.equipment[item].itemdefinition.slot].area.p1.x+this.equipmentslots[hero.equipment[item].itemdefinition.slot].padding.x,
+													this.equipmentslots[hero.equipment[item].itemdefinition.slot].area.p1.y +this.equipmentslots[hero.equipment[item].itemdefinition.slot].padding.y,
+														100,100,hero.equipment[item]), hero.equipment[item].itemdefinition.type);
+
+			this.equipmentslots[hero.equipment[item].itemdefinition.slot].setContent(dg)
+			this.entities.push(dg);
+		}
+	}
+
 }
