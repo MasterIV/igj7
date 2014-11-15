60738d4350788a736bfa71d46177c8561b9f6aab
Kevin Kub <kevin@Kevins-MacBook-Air.local>
2014-11-15 14:29:00 +0100

    Added scaling to sprite, make encounter zoomable..

--- js/lib/sprite.js
+++ js/lib/sprite.js
@@ -4,12 +4,12 @@ function sprite( img ) {
 	this.height = this.img.height;
 }
 
-sprite.prototype.draw = function( ctx, x, y ) {
-	ctx.drawImage( this.img, x|0, y|0 );
+sprite.prototype.draw = function( ctx, x, y, scaleX, scaleY ) {
+	ctx.drawImage( this.img, x|0, y|0, this.width * (scaleX||1), this.height * (scaleY||1));
 };
 
-sprite.prototype.center = function( ctx, x, y ) {
-	ctx.drawImage( this.img, (x-this.img.width/2)|0, (y-this.img.height/2)|0 );
+sprite.prototype.center = function( ctx, x, y, scaleX, scaleY ) {
+	ctx.drawImage( this.img, (x-this.img.width*(scaleX||1)/2)|0, (y-this.img.height*(scaleY||1)/2)|0, this.img.width*scaleX, this.img.height*scaleY );
 };
 
 sprite.prototype.area = function( ctx, sx, sy, sw, sh, x, y ) {
@@ -23,10 +23,10 @@ function AnimationSprite( img, frames ) {
 	this.f = frames;
 }
 
-AnimationSprite.prototype.draw = function( ctx, x, y, f ) {
-	ctx.drawImage( this.img, f*this.w, 0, this.w, this.h, x|0, y|0, this.w, this.h );
+AnimationSprite.prototype.draw = function( ctx, x, y, f, scaleX, scaleY ) {
+	ctx.drawImage( this.img, f*this.w, 0, this.w * (scaleX||1), this.h * (scaleY||1), x|0, y|0, this.w, this.h );
 };
 
-AnimationSprite.prototype.center = function( ctx, x, y, f ) {
-	ctx.drawImage( this.img, f*this.w, 0, this.w, this.h, (x-this.w/2)|0, (y-this.h/2)|0, this.w, this.h );
+AnimationSprite.prototype.center = function( ctx, x, y, f, scaleX, scaleY ) {
+	ctx.drawImage( this.img, f*this.w, 0, this.w * (scaleX||1), this.h * (scaleY||1), (x-this.w/2)|0, (y-this.h/2)|0, this.w, this.h );
 };
