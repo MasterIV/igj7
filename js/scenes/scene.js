76f9cc5135e0f2bd2da3d3ae1109712dda44c17b
Tobias Rojahn <tr@wlec.ag>
2014-11-15 09:35:56 +0100

    Mock hero interface

--- js/scenes/scene.js
+++ js/scenes/scene.js
@@ -1,8 +1,15 @@
 function scene() {
+	this.blocking = [];
 	this.entities = [];
 };
 
 scene.prototype.update = function( delta ) {
+	if( this.blocking.length ) {
+		if( this.blocking[0].update( delta ))
+			this.blocking.shift();
+		return;
+	}
+
 	for( var i = 0; i < this.entities.length; i++ )
 		if( this.entities[i].update )
 			this.entities[i].update( delta );
@@ -18,18 +25,33 @@ scene.prototype.draw = function( ctx ) {
 }
 
 scene.prototype.click = function( pos ) {
+	if( this.blocking.length ) {
+		this.blocking[0].click( pos );
+		return;
+	}
+
 	for( var i = 0; i < this.entities.length; i++ )
 		if( this.entities[i].click )
 			this.entities[i].click( pos );
 }
 
 scene.prototype.mousedown = function( pos ) {
+	if( this.blocking.length ) {
+		this.blocking[0].mousedown( pos );
+		return;
+	}
+
 	for( var i = 0; i < this.entities.length; i++ )
 		if( this.entities[i].mousedown )
 			this.entities[i].mousedown( pos );
 }
 
 scene.prototype.mouseup = function( pos ) {
+	if( this.blocking.length ) {
+		this.blocking[0].mouseup( pos );
+		return;
+	}
+
 	for( var i = 0; i < this.entities.length; i++ )
 		if( this.entities[i].mouseup )
 			this.entities[i].mouseup( pos );
