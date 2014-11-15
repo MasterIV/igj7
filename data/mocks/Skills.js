20577d760d0afcdd66a1bb172f38eb848e489f8e
purhur <purhur@purh.pw>
2014-11-15 09:56:19 +0100

    Changed mock file types

---
+++ data/mocks/Skills.js
@@ -0,0 +1,17 @@
+var skillDefinitions = {
+  "1": {
+    "name": "Mächtige Hand",
+    "image": "item.png",
+    "effects": [
+      "heal10", "damage10"
+    ]
+  },
+  "2": {
+    "name": "Reib die Gurke",
+    "image": "item2.png",
+    "effects": [
+      "damage10"
+    ]
+  } 
+};
+
