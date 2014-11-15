5e8416152333188273ca5152ba534954e29d6bdf
purhur <purhur@purh.pw>
2014-11-15 21:23:00 +0100

    switch scene

--- js/scenes/loose.js
+++ js/scenes/loose.js
@@ -1,17 +1,28 @@
 function looseScene() {
-	this.bg = game.scene.map.bg;
+	this.bg = new sprite( 'img/maps/mountain.jpg' );
 
-	this.blocking.push(
-		new dialogue('DU BIst leider gestorben',
-			[	{text: 'OK',
+	this.entities = [];
+
+	var fee = new sprite('img/characters/fee.png');
+	this.entities.push({
+		draw: function (ctx) {
+			fee.draw(ctx, 800,200)
+		}
+	});
+
+	var messageBox = new dialogue('DU BIst leider gestorben',
+			[{text: 'OK',
 				callback: function() {
 					game.scene = scenes.menu;
-				}},
-				{text: 'NEIN!',
+			}},
+			{text: 'NEIN!',
 				callback: function() {
 					game.scene = scenes.character;
-				}}, ])
-	)
+				}}, ], (game.display.height/4)*3);
+
+	this.blocking.push(messageBox);
 }
 
 looseScene.prototype = new scene();
+
+
