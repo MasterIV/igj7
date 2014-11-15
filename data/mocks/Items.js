782c77bad99dbb85026d8c8dfa0e201d42c11d29
purhur <purhur@purh.pw>
2014-11-15 09:58:13 +0100

    Changed data mocks

--- data/mocks/Items.js
+++ data/mocks/Items.js
@@ -1,23 +1,25 @@
 var itemDefinitions = {
-  "1": {
-    "itemname": "Dampfhammeruhr 3000",
-    "type": "shield",
-    "dmg": 1,
-    "def": 1,
-    "int": 1,
-    "dex": 1,
-    "const": 1,
-    "blingbling": 1
-  },
-  "2": {
-    "itemname": "Klobürste",
-    "type": "sword",
-    "dmg": 24,
-    "def": 2,
-    "int": 1,
-    "dex": 5,
-    "const": 1,
-    "blingbling": 1
-  }
+    "1": {
+        "itemname": "Dampfhammeruhr 3000",
+        "sprite":"mock/button.png",
+        "type": "shield",
+        "dmg": 1,
+        "def": 1,
+        "int": 1,
+        "dex": 1,
+        "const": 1,
+        "blingbling": 1
+    },
+    "2": {
+        "itemname": "Klobürste",
+        "sprite":"mock/button.png",
+        "type": "sword",
+        "dmg": 24,
+        "def": 2,
+        "int": 1,
+        "dex": 5,
+        "const": 1,
+        "blingbling": 1
+}
 };
 
