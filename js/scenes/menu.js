769584f9c4aebeef07f03c0fee6c498856795f5b
Tobias Rojahn <tr@wlec.ag>
2014-11-16 05:30:03 +0100

    main menu

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -1,27 +1,19 @@
 function menuScene() {
 	this.entities = [
-		new textButton('Map', 10, 10, 200, 30, function () {
+		new textButton('Start Game', 10, 10, 200, 30, function () {
 			scenes.map.goToFinalScene();
 			game.scene = scenes.map;
 		}, {}),
-		new textButton('Kampf', 10, 50, 200, 30, function () {
+
+		new textButton('Debug', 10, 50, 200, 30, function () {
 			/** debug code to have items from beginning */
-			//for(var key in itemDefinitions)
-			//	hero.loot(new item(itemDefinitions[key]));
+			for(var key in itemDefinitions)
+				hero.loot(new item(itemDefinitions[key]));
 			for(var key in skillDefinitions)
 				hero.skills.push(skillDefinitions[key]);
 
 			scenes.combat.setEnemies(['roboter','roboter','roboter']);
 			game.scene = scenes.combat;
-		}, {}),
-		new textButton('Character', 10, 90, 200, 30, function () {
-			game.scene = scenes.character;
-		}, {}),
-		new textButton('TOD', 10, 130, 200, 30, function () {
-			game.scene =  new looseScene();;
-		}, {}),
-		new textButton('switch', 10, 170, 200, 30, function () {
-			game.scene =  new switchScene(scenes.map, 2000, 'MAP');
 		}, {})
 	];
 }
