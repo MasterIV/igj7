31593aeb0c03509d672cc0e717eace0a5dd8895d
Trokan <mail@simonpontius.de>
2014-11-16 01:18:39 +0100

    Skills.js update

--- data/mocks/Skills.js
+++ data/mocks/Skills.js
@@ -1,68 +1,76 @@
 var skillDefinitions = {
-	"attack": {
-		"name": "Angriff",
-		"costs": 0,
-		"target": "single",
-		"effects": {
-			"Attack": {"factor": 1}
-		}
-	},
-	"heal": {
+	"heal_self": {
 		"name": "Heilung",
-		"costs": "1",
+		"costs": "3",
 		"target": "self",
 		"effects": {
 			"Heal": {"base": 20, "attr": "int", "rnd": .6 }
 		}
 	},
-	"double_damage": {
-		"name": "Doppelter Schaden",
-		"costs": "1",
-		"target": "all",
-		"effects": {
-			"Attack": {"factor": 2}
-		}
-	},
 	"heal_enemy": {
-		"name": "Gegner Heilen",
+		"name": "Gegner heilen (warum auch immer?!)",
 		"costs": "1",
-		"target": "all",
+		"target": "single",
 		"effects": {
 			"Heal": {"base": 20, "attr": "int", "rnd": .6 }
 		}
 	},
-	"stun": {
-		"name": "Stunnen",
+	"heal_w_dmg": {
+		"name": "Blaues Auge der Heilung",
 		"costs": "1",
-		"target": "all",
+		"target": "self",
 		"effects": {
-			"Stun": {"duration": 3, "attr": "dex" }
+			"Heal": {"base": 40, "attr": "int", "rnd": .6 }
+			"Attack": {"factor": 0.2}
 		}
 	},
-	"hot": {
-		"name": "Hot",
-		"costs": "1",
-		"target": "all",
-		"effects": {
-			"Buff": {"duration": 3, "value": -15 }
-		}
-	},
-	"dot": {
-		"name": "Dot",
-		"costs": "1",
-		"target": "all",
-		"effects": {
-			"Buff": {"duration": 3, "value": "5" }
-		}
-	},
-	"shield": {
-		"name": "Shield",
-		"costs": "1",
-		"target": "all",
+	"buff_cucumber": {
+		"name": "Reib die Gurke",
+		"costs": "2",
+		"target": "self",
 		"effects": {
 			"Buff": {"duration": 3, "value": "5" }
 		}
 	}
-
+	"attack_god": {
+		"name": "Faust Gottes",
+		"costs": "10",
+		"target": "all",
+		"effects": {
+			"Attack": {"factor": 1.5}
+		}
+	},
+	"attack_fist": {
+		"name": "Mächtige Hand",
+		"costs": "5",
+		"target": "single",
+		"effects": {
+			"Attack": {"factor": 2.5}
+		}
+	},
+	"attack_rage": {
+		"name": "Rage!#$§!?%",
+		"costs": "4",
+		"target": "single",
+		"effects": {
+			"Attack": {"factor": 2.3}
+		}
+	},
+	"double_damage": {
+		"name": "Doppelter Schaden",
+		"costs": "3",
+		"target": "single",
+		"effects": {
+			"Attack": {"factor": 2}
+		}
+	},
+	"stun_leg": {
+		"name": "Bein stellen",
+		"costs": "2",
+		"target": "single",
+		"effects": {
+			"Stun": {"duration": 3, "attr": "dex" }
+		}
+	},
 };
 
